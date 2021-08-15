cf0ed0e1cf2d4b40aff736968573c52e3fd9024a
bagasys <bagasys3@gmail.com>
2021-08-16 05:12:23 +0700

    RED user's valid? method should return false when email is an empty string.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -93,6 +93,18 @@ describe User do
         )
         expect(user.valid?).to be false
       end
+
+      it 'should return false when email is an empty string' do
+        user_data = @users_data[0]
+        user = User.new(
+          id: user_data[:id],
+          email: '',
+          username: user_data[:username],
+          bio: user_data[:bio],
+          created_at: user_data[:created_at],
+        )
+        expect(user.valid?).to be false
+      end
     end
   end
 
