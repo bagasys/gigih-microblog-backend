b2012e4867a86937b30362723eedb766a6b6709f
bagasys <bagasys3@gmail.com>
2021-08-16 07:29:46 +0700

    GREEN

--- models/user.rb
+++ models/user.rb
@@ -21,6 +21,7 @@ class User
   end
 
   def save    
+    return false unless self.valid?
     client = create_db_client
     client.query(
       "INSERT INTO users (username, email, bio) VALUES ('#{@username}', '#{@email}', '#{@bio}')"
