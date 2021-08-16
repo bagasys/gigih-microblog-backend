883dafd67d8333163e7d1d8fd654b3efa2b323d5
bagasys <bagasys3@gmail.com>
2021-08-16 16:37:20 +0700

    GREEN

---
+++ models/post.rb
@@ -0,0 +1,15 @@
+require_relative '../db/connector.rb'
+
+class Post
+  attr_accessor :text_content, :attachment
+  attr_reader :id, :user_id, :created_at 
+
+  def initialize(params)
+    @id = params[:id]
+    @user_id = params[:user_id]
+    @text_content = params[:text_content]
+    @attachment = params[:attachment]
+    @created_at = params[:created_at]
+  end
+
+end
