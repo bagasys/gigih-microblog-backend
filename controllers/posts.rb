dab72f7f713482bd14f2394d0fd267d0fe1616af
bagasys <bagasys3@gmail.com>
2021-08-17 23:45:19 +0700

    GREEN

---
+++ controllers/posts.rb
@@ -0,0 +1,26 @@
+require './models/post'
+require './models/hashtag'
+require "sinatra"
+
+class PostsController
+  def create(params)
+    post = Post.new(user_id: params[:user_id], text_content: params[:text_content], parent_id: params[:parent_id], attachment: params[:attachment])
+
+    if post.save
+      Hashtag::save_hashtags_from_post(post.text_content, post.id)
+      return ({
+      status: 201,
+      message: "success",
+      data: {
+        id: post.id,
+        parent_id: post.parent_id,
+        user_id: post.user_id,
+        text_content: post.text_content,
+        attachment: post.attachment,
+        created_at: post.created_at
+      }
+    }).to_json
+    end
+  end
+
+end
