67b2aff74de3877b5781c7c0239beb9ece0b3607
bagasys <bagasys3@gmail.com>
2021-08-17 20:50:00 +0700

    GREEN

--- controllers/users.rb
+++ controllers/users.rb
@@ -1,8 +1,23 @@
 
 require './models/user'
 require "sinatra"
+
 class UsersController
   def create(params)
+    user = User.new(username: params[:username], email: params[:email], bio: params[:bio])
     
+    if user.save
+      return ({
+      status: 201,
+      message: "success",
+      data: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        bio: user.bio,
+        created_at: user.created_at
+      }
+    })
+    end
   end
 end
