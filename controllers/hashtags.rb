f29b9e87dbcd89d56331a5bb34eab6df0086f494
bagasys <bagasys3@gmail.com>
2021-08-19 17:24:15 +0700

    GREEN

--- controllers/hashtags.rb
+++ controllers/hashtags.rb
@@ -4,6 +4,17 @@ require "sinatra"
 
 class HashtagsController
   def show_trending_hashtags()
+    hashtags = Hashtag::find_trendings()
     
+    data = []
+    hashtags.each do |hashtag|
+      data << {name: hashtag.name, total_occurences: hashtag.total_occurences}
+    end
+
+    return ({
+      status: 200,
+      message: "success",
+      data: data
+    }).to_json
   end
 end
